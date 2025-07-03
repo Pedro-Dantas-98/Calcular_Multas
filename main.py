9947919bbedabd076f857f809967802b0f985896
Pedro Dantas <pedrodantas@live.com.pt>
2025-07-03 13:58:08 +0100

    Condição de encerro e ajustes gerais

--- main.py
+++ main.py
@@ -1,51 +1,64 @@
 def multaLocalidade(velocidade: int):
-    if velocidade > 50:
-        print("Multa a atribuir: 60€")
+    if velocidade >= 120:
+        print("Multa a atribuir: 320€") 
     elif velocidade >= 90:
         print("Multa a atribuir: 120€")
-    elif velocidade >= 120:
-        print("Multa a atribuir: 320€")                
+    elif velocidade > 50:
+        print("Multa a atribuir: 60€")                
     else:
         print("Não ocorreu qualquer infração.")
         
 def multaFora(velocidade: int):
-    if velocidade > 90:
-        print("Multa a atribuir: 60€")
-    elif velocidade >= 120:
-        print("Multa a atribuir: 120€")                
+    if velocidade >= 120:
+        print("Multa a atribuir: 120€")
+    elif velocidade > 90:
+        print("Multa a atribuir: 60€")               
     else:
         print("Não ocorreu qualquer infração.")
         
 def multaAutoestrada(velocidade: int):
-    if velocidade > 120:
-        print("Multa a atribuir: 60€")
+    if velocidade >= 175:
+        print("Multa a atribuir: 360€")
     elif velocidade > 150:
         print("Multa a atribuir: 120€")
-    elif velocidade >= 175:
-        print("Multa a atribuir: 360€")                
+    elif velocidade > 120:
+        print("Multa a atribuir: 60€")                
     else:
-        print("Não ocorreu qualquer infração.")  
+        print("Não ocorreu qualquer infração.")        
 
 while True:
-    print("Cálculo de Multas\n")
-    print("Onde circulava o veículo?") 
-    print("1 - Localidade")
-    print("2 - Fora da Localidade") 
-    print("3 - Autoestrada")
-    print("4 - Cancelar Cálculo")
-    print()
+    try:
+        print("Cálculo de Multas")
+        print("-------------------------")
+        print("Onde circulava o veículo?") 
+        print("1 - Localidade")
+        print("2 - Fora da Localidade") 
+        print("3 - Autoestrada")
+        print()         
+            
+        local = input("Selecione o local: ")
         
-    local = input("Selecione o local: ")
+        velocidade = int(input("Introduza a velocidade a que circulava o veículo (km/h): "))
+            
+        if local == "1":
+            multaLocalidade(velocidade)
+        elif local == "2":
+            multaFora(velocidade)
+        elif local == "3":
+            multaAutoestrada(velocidade)                           
+        else:
+            print("Local inválido.")
     
-    velocidade = int(input("Introduza a velocidade a que circulava o veículo (km/h): "))
+        encerro = input("Quer encerrar o programa? (S/N): ").lower()
         
-    if local == "1":
-        multaLocalidade(velocidade)
-    elif local == "2":
-        multaFora(velocidade)
-    elif local == "3":
-        multaAutoestrada(velocidade)
-    elif local == "4":
-        continue                             
-    else:
-        print("Local inválido.")
+        if encerro == "s":
+            print("A encerrar o programa.")
+            print()
+            break
+        else:
+            print("A resumir o programa.")
+            print()             
+
+    except ValueError:
+        print("A velocidade do véiculo tem de ser um número.")
+        print()        
